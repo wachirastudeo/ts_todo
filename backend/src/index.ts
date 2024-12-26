fab2d6c41dab6a12fbbdd637df1f341eec08651e
wachira <wachirastudeo@gmail.com>
2024-12-26 07:43:13 +0700

    setup file

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+
+const app = express();
+
+const port= Number(process.env.PORT || 3000)
+
+
+app.listen(port, () => {
+    console.log("Server is running on port 3000");
+});
+
